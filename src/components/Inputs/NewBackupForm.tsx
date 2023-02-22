83217ee9a863badaf5f392a71c40d99d1ed9af99
Theo F <theo.farnole@gmail.com>
2023-02-22 14:55:25 +0100

    ADD form errors

--- src/components/Inputs/NewBackupForm.tsx
+++ src/components/Inputs/NewBackupForm.tsx
@@ -4,11 +4,17 @@ import useGetWorkspaces from '../../hooks/useGetWorkspaces';
 import { loadingStr } from '../../types/Loading';
 import PathInput from './PathInput';
 
+interface FormErrors {
+    workspaceError?: string;
+    pathError?: string;
+}
+
 export function NewBackupForm() {
     const userWorkspaces = useGetWorkspaces();
 
     const [selectedWorkspaceID, selectWorkspaceID] = React.useState<string | undefined>(undefined);
-    const [backupPath, setBackupPath] = React.useState<string | undefined>(undefined);
+    const [backupPath, setBackupPath] = React.useState<string>("");
+    const [inputErrors, setInputErrors] = React.useState<FormErrors>({});
 
     const selectedWorkspace = userWorkspaces !== loadingStr ? userWorkspaces
         .find(w => w.id === selectedWorkspaceID) : undefined;
@@ -26,6 +32,7 @@ export function NewBackupForm() {
         <FormControl fullWidth className="d-flex flex-column" sx={{ gap: 3 }}>
             <InputLabel id={SELECT_WORKSPACE_ID_LABEL}>Workspace</InputLabel>
             <Select
+                error={inputErrors.workspaceError !== undefined}
                 labelId={SELECT_WORKSPACE_ID_LABEL}
                 id={SELECT_WORKSPACE_ID}
                 value={selectedWorkspace?.name}
@@ -40,12 +47,46 @@ export function NewBackupForm() {
 
             </Select>
 
-            <PathInput path={backupPath ?? ""} onChange={(path) => setBackupPath(path)} />
+            <PathInput
+                error={inputErrors.pathError !== undefined}
+                helperText={inputErrors.pathError}
+                path={backupPath}
+                onChange={(path) => setBackupPath(path)}
+            />
 
-            <Button variant="contained">
+            <Button variant="contained" onClick={create}>
                 Create
             </Button>
 
         </FormControl>
     </>;
+
+    function create() {
+        const errors = detectErrors(backupPath, selectedWorkspaceID);
+
+        if (errors) {
+            console.log(errors);
+            setInputErrors(errors)
+        }
+        else {
+            setInputErrors({});
+            throw "not implemented";
+        }
+    }
+
+
+}
+
+function detectErrors(path: string, workspaceId: string | undefined): FormErrors | undefined {
+    const errors: FormErrors = {};
+
+    if (!path) {
+        errors.pathError = "Path is required";
+    }
+
+    if (!workspaceId) {
+        errors.workspaceError = "Workspace is required";
+    }
+
+    return Object.keys(errors).length > 0 ? errors : undefined;
 }
--- src/components/Inputs/PathInput.tsx
+++ src/components/Inputs/PathInput.tsx
@@ -4,15 +4,26 @@ import Button from "@mui/material/Button";
 interface Props {
     path: string;
     onChange: (path: string) => void
+    error?: boolean;
+    helperText?: string;
 }
 
 export default function PathInput({
     path,
-    onChange
+    onChange,
+    error,
+    helperText
 }: Props) {
 
     return <div className="d-flex">
-        <TextField value={path} label="Path" className="flex-grow-1" onChange={(e) => onChange(e.target.value)} />
+        <TextField
+            error={error}
+            helperText={helperText}
+            value={path}
+            label="Path"
+            className="flex-grow-1"
+            onChange={(e) => onChange(e.target.value)}
+        />
         <Button onClick={onClick}>
             Select a path
         </Button>
