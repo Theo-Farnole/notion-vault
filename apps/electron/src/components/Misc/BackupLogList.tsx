1da04ea11a6fbf53d8432e3ef566c76d7dbd4781
Theo F <theo.farnole@gmail.com>
2023-03-09 17:06:36 +0100

    ADD human readable date + RETAKE replace end time by duration in BackupLogList

--- apps/electron/src/components/Misc/BackupLogList.tsx
+++ apps/electron/src/components/Misc/BackupLogList.tsx
@@ -1,4 +1,5 @@
 import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material"
+import React from "react"
 import { BackupLog } from "../../types/BackupMetadata"
 
 interface Props {
@@ -6,30 +7,46 @@ interface Props {
 }
 
 export const BackupLogList = ({ backupsLogs }: Props) => {
+
+    const sortedLogs = React.useMemo(() => backupsLogs.sort(b => -b.startTimestamp), [backupsLogs])
+
     return <TableContainer component={Paper}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
             <TableHead>
                 <TableRow>
                     <TableCell>Start</TableCell>
-                    <TableCell>End</TableCell>
+                    <TableCell>Duration</TableCell>
                     <TableCell align="right">Type</TableCell>
                 </TableRow>
             </TableHead>
             <TableBody>
 
-                {backupsLogs.map((backupLog) => (
-                    <TableRow
-                        key={backupLog.startTimestamp}
-                        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                    >
-                        <TableCell component="th" scope="row">
-                            {backupLog.startTimestamp}
-                        </TableCell>
-                        <TableCell>{backupLog.endTimestamp}</TableCell>
-                        <TableCell align="right">{backupLog.type}</TableCell>
-                    </TableRow>
-                ))}
+                {sortedLogs
+                    .map((backupLog) => (
+                        <TableRow
+                            key={backupLog.startTimestamp}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {dateToHumanReadable(backupLog.startTimestamp)}
+                            </TableCell>
+                            <TableCell>
+                                {durationToHumanReadable(backupLog.endTimestamp - backupLog.startTimestamp)} s
+                            </TableCell>
+                            <TableCell align="right">{backupLog.type}</TableCell>
+                        </TableRow>
+                    ))}
             </TableBody>
         </Table>
     </TableContainer>
+
+    function dateToHumanReadable(timestamp: number): string {
+        const date = new Date(timestamp);
+
+        return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+    }
+
+    function durationToHumanReadable(durationTimestamp: number) {
+        return (durationTimestamp / 1000).toFixed(0);
+    }
 }
