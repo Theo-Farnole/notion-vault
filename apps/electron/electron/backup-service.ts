a81b31147495209d2b5bae66729decba01272353
Theo F <theo.farnole@gmail.com>
2023-03-09 16:55:04 +0100

    ADD backup log when a backup is made

--- apps/electron/electron/backup-service.ts
+++ apps/electron/electron/backup-service.ts
@@ -40,39 +40,7 @@ export class BackupService {
         const startTimestamp = Date.now();
         const folder = join(backup.savePath, startTimestamp.toString());
 
-        const config: AxiosRequestConfig = {
-            baseURL: "https://api.notion.com/v1/",
-            headers: {
-                'Authorization': `Bearer ${backup.workspace.accessToken}`,
-                'Notion-Version': '2022-06-28',
-                'Content-Type': 'application/json',
-            }
-        };
-        const client = axios.create(config)
-
-
-        const response = await client.post('search', {}, config);
-
-        for (const block of response.data.results) {
-
-            if (!existsSync(folder)) {
-                mkdirSync(folder);
-            }
-
-            writeFileSync(`${folder}/${block.id}.json`, JSON.stringify(block));
-
-            const { data: { ressults: childBlocks } } = await client.get(`blocks/${block.id}/children`);
-
-            if (childBlocks) {
-                const childFolder = join(folder, block.id);
-                mkdirSync(childFolder);
-
-                for (const child of childBlocks) {
-                    const childFile = join(childFolder, child.id + ".json");
-                    writeFileSync(childFile, child.id);
-                }
-            }
-        }
+        await writeBackup();
 
         const endTimestamp = Date.now();
 
@@ -86,5 +54,42 @@ export class BackupService {
         this.settings.replaceBackup(backup, newBackup);
 
         console.log(`Backup ${startTimestamp} is finished`);
+
+        async function writeBackup() {
+            const config: AxiosRequestConfig = {
+                baseURL: "https://api.notion.com/v1/",
+                headers: {
+                    'Authorization': `Bearer ${backup.workspace.accessToken}`,
+                    'Notion-Version': '2022-06-28',
+                    'Content-Type': 'application/json',
+                }
+            };
+            const client = axios.create(config)
+
+
+            const response = await client.post('search', {}, config);
+
+            for (const block of response.data.results) {
+
+                if (!existsSync(folder)) {
+                    mkdirSync(folder);
+                }
+
+                writeFileSync(`${folder}/${block.id}.json`, JSON.stringify(block));
+
+                const { data: { ressults: childBlocks } } = await client.get(`blocks/${block.id}/children`);
+
+                if (childBlocks) {
+                    const childFolder = join(folder, block.id);
+                    mkdirSync(childFolder);
+
+                    for (const child of childBlocks) {
+                        const childFile = join(childFolder, child.id + ".json");
+                        writeFileSync(childFile, child.id);
+                    }
+                }
+            }
+
+        }
     }
 }
--- apps/electron/src/hooks/storage/useGetSavedBackup.tsx
+++ apps/electron/src/hooks/storage/useGetSavedBackup.tsx
@@ -2,15 +2,22 @@ import React from "react";
 import { electronApi } from "../../const";
 import { BackupMetadata } from "../../types/BackupMetadata";
 import { loadingStr, LoadingStr } from "../../types/Loading";
+import { useOnStorageUpdate } from "./useOnStorageUpdate";
 
 export function useGetSavedBackups() {
 
     const [backups, setBackups] = React.useState<LoadingStr | BackupMetadata[]>(loadingStr);
 
+    useOnStorageUpdate(getBackups);
+
     React.useEffect(() => {
-        electronApi.storage.backups.get()
-            .then((b: any) => setBackups(b || []));
+        getBackups();
     }, []);
 
     return backups;
+
+    function getBackups() {
+        electronApi.storage.backups.get()
+            .then((b: any) => setBackups(b || []));
+    }
 }
