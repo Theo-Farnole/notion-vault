b23859acf14d89159da0764c463a1ce5da2b0f08
Theo F <theo.farnole@gmail.com>
2023-04-14 14:23:21 +0200

    ADD logo in header

--- apps/website/src/components/Layout/Header.tsx
+++ apps/website/src/components/Layout/Header.tsx
@@ -1,13 +1,17 @@
 import Content from "./Content";
 import style from "./Header.module.scss";
+import Logo from "../../assets/logo192.png";
 
 export default function Header() {
     return <header className={style.root}>
 
         <Content className={style.content}>
-            <span className={style.productName}>
-                Notion Vault
-            </span>
+            <div className={style.product}>
+                <img src={Logo} height={30} alt="software logo" />
+                <span className={style.productName}>
+                    Notion Vault
+                </span>
+            </div>
 
 
             <a className={style.downloadBtn} href="#download">
